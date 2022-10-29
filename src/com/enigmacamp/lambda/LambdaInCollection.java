d55a4ee5eda4a8b84bd2fbb00e56e1e31cf6a47e
edwardsuwirya <edward_suwirya@yahoo.com>
2022-10-29 08:37:54 +0700

    Another lambda example in collection

---
+++ src/com/enigmacamp/lambda/LambdaInCollection.java
@@ -0,0 +1,32 @@
+package com.enigmacamp.lambda;
+
+import java.util.ArrayList;
+import java.util.Collections;
+import java.util.List;
+import java.util.stream.Stream;
+
+public class LambdaInCollection {
+    public static void main(String[] args) {
+        List<String> list = new ArrayList<>();
+        list.add("Fajri");
+        list.add("Fadli");
+        list.add("Agus");
+
+        System.out.println("For Each");
+        list.forEach(
+                (nama) -> System.out.println(nama)
+        );
+
+        System.out.println("Sorting");
+        list.sort((nama1, nama2) -> {
+            return nama1.compareTo(nama2);
+        });
+        list.forEach(
+                (nama) -> System.out.println(nama)
+        );
+
+        System.out.println("Filtering");
+        Stream result = list.stream().filter((nama) -> nama.startsWith("A"));
+        result.forEach(n -> System.out.println(n));
+    }
+}
