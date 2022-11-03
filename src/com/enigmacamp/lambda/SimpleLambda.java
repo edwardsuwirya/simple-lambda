25b1f49c982df196314b94c6612c271572c4ebd0
edwardsuwirya <edward_suwirya@yahoo.com>
2022-11-03 14:58:35 +0700

    Built in functional interface

--- src/com/enigmacamp/lambda/SimpleLambda.java
+++ src/com/enigmacamp/lambda/SimpleLambda.java
@@ -1,5 +1,9 @@
 package com.enigmacamp.lambda;
 
+import java.util.function.Consumer;
+import java.util.function.Function;
+import java.util.function.Predicate;
+
 @FunctionalInterface
 interface Printable {
     void print(String message);
@@ -12,17 +16,36 @@ interface MesinHitung {
 
 public class SimpleLambda {
     public static void main(String[] args) {
-        Printable printable = (message) -> {
-            System.out.println(message);
-        };
-        printable.print("Enigma");
+//        Printable printable = (message) -> {
+//            System.out.println(message);
+//        };
+//        printable.print("Enigma");
+//
+//        MesinHitung penjumlahan = (angka1, angka2) -> angka1 + angka2;
+//        int hasilPenjumlahan = penjumlahan.hitung(10, 3);
+//        printable.print(String.valueOf(hasilPenjumlahan));
+//
+//        MesinHitung perkalian = (int1, int2) -> int1 * int2;
+//        int hasilPerkalian = perkalian.hitung(2, 3);
+//        printable.print(String.valueOf(hasilPerkalian));
 
-        MesinHitung penjumlahan = (angka1, angka2) -> angka1 + angka2;
-        int hasilPenjumlahan = penjumlahan.hitung(10, 3);
-        printable.print(String.valueOf(hasilPenjumlahan));
+        Predicate<String> isALongWord = w -> w.length() > 10;
+        String word = "EnigmaCamp Ragunan";
+        boolean result = isALongWord.test(word);
+        System.out.println(result);
 
-        MesinHitung perkalian = (int1, int2) -> int1 * int2;
-        int hasilPerkalian = perkalian.hitung(2, 3);
-        printable.print(String.valueOf(hasilPerkalian));
+        Consumer<Student> updateName = c -> c.setName(c.getName().toUpperCase());
+        Student student = new Student();
+        student.setName("Fadli");
+        updateName.accept(student);
+        System.out.println(student);
+
+        int n = 5;
+        modifyValue(n, num -> num + 10);
+    }
+
+    static void modifyValue(int v, Function<Integer, Integer> function) {
+        int result = function.apply(v);
+        System.out.println(result);
     }
 }
